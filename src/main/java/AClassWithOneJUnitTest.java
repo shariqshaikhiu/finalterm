c56da479c95ebf528538483c515facbb38b69f85
Afiiii-h <afifahabib2013@gmail.com>
2025-01-16 16:10:26 +0500

    Question 1

---
+++ src/main/java/AClassWithOneJUnitTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class AClassWithOneJUnitTest {
+
+    @Test
+    void demoTestMethod() {
+        assertTrue(true);
+    }
+}
